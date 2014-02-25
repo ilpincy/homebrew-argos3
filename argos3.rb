1f5972032e144b4701349de4d360ea076311cbe9
Carlo Pinciroli <cpinciro@ulb.ac.be>
2014-02-25 23:31:54 +0100

    Added formula for argos3. Added brief README file.

---
+++ argos3.rb
@@ -0,0 +1,36 @@
+require "formula"
+
+class Argos3 < Formula
+  homepage "http://iridia.ulb.ac.be/argos/"
+  url "https://github.com/ilpincy/argos3/archive/3.0.0-beta22.tar.gz"
+  sha1 "be8ec6048648505e9b86ef529ecc3e2e01abb0fc"
+  version "3.0.0-beta22"
+  head "https://github.com/ilpincy/argos3.git"
+
+  option 'without-qt', 'Compile without the Qt-OpenGL visualization'
+
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "docbook" => :build
+  depends_on "asciidoc" => :build
+  depends_on "graphviz" => :build
+  depends_on "doxygen" => :build
+  depends_on "gsl"
+  depends_on "libpng"
+  depends_on "freeimage"
+  depends_on "lua" => :recommended
+  depends_on "qt" => :recommended
+
+  def install
+    mkdir "build_simulator"
+    cd "build_simulator"
+    system "cmake", "../src", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DARGOS_BUILD_NATIVE=ON"
+    system "make"
+    system "make doc"
+    system "make install"
+  end
+
+  test do
+    system "argos3 -q all"
+  end
+end
