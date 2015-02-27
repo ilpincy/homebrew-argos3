b9fa0054ac3d7169e658b940f4994542210e6cb5
Carlo Pinciroli <cpinciro@ulb.ac.be>
2015-02-27 12:15:26 -0500

    Formula for new version of ARGoS3 (3.0.0-beta30)

--- argos3.rb
+++ argos3.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Argos3 < Formula
   homepage "http://iridia.ulb.ac.be/argos/"
-  url "https://github.com/ilpincy/argos3/archive/3.0.0-beta29.tar.gz"
-  sha1 "282b7c9c7630354448ae21cab356567da31c970c"
-  version "3.0.0-beta29"
+  url "https://github.com/ilpincy/argos3/archive/3.0.0-beta30.tar.gz"
+  sha1 "db51a3b7e9b50794c9808b842f74a61c3688fd22"
+  version "3.0.0-beta30"
   head "https://github.com/ilpincy/argos3.git"
 
   option 'without-qt', 'Compile without the Qt-OpenGL visualization'
@@ -20,6 +20,7 @@ class Argos3 < Formula
   depends_on "freeimage"
   depends_on "lua51" => :recommended
   depends_on "qt" => :recommended
+  depends_on "bash-completion" => :recommended
 
   def install
     mkdir "build_simulator"
