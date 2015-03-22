e5b1ed119cfb7f881c6f06140d88d11031fa220f
Carlo Pinciroli <cpinciro@ulb.ac.be>
2015-03-22 19:55:02 +0100

    Formula for new version of ARGoS3 (3.0.0-beta32)

--- argos3.rb
+++ argos3.rb
@@ -1,10 +1,16 @@
 require "formula"
 
 class Argos3 < Formula
+  VERSION_MAJOR   = "3"
+  VERSION_MINOR   = "0"
+  VERSION_PATCH   = "0"
+  VERSION_RELEASE = "beta32"
+  VERSION_FULL    = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}-#{VERSION_RELEASE}"
+  
   homepage "http://www.argos-sim.info/"
-  url "https://github.com/ilpincy/argos3/archive/3.0.0-beta31.tar.gz"
-  sha1 "1dea6fa9aa7d0ca3aaf833c48724710bcb27b114"
-  version "3.0.0-beta31"
+  url "https://github.com/ilpincy/argos3/archive/#{VERSION_FULL}.tar.gz"
+  sha1 "ecf832c9955afdad51cde66c2a1496f2254095b8"
+  version VERSION_FULL
   head "https://github.com/ilpincy/argos3.git"
 
   option 'without-qt', 'Compile without the Qt-OpenGL visualization'
@@ -25,7 +31,7 @@ class Argos3 < Formula
   def install
     mkdir "build_simulator"
     cd "build_simulator"
-    system "cmake", "../src", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DARGOS_BUILD_NATIVE=ON"
+    system "cmake", "../src", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DARGOS_BUILD_NATIVE=ON", "-DCPACK_PACKAGE_VERSION_MAJOR=#{VERSION_MAJOR}", "-DCPACK_PACKAGE_VERSION_MINOR=#{VERSION_MINOR}", "-DCPACK_PACKAGE_VERSION_PATCH=#{VERSION_PATCH}", "-DCPACK_PACKAGE_RELEASE=#{VERSION_RELEASE}"
     system "make"
     system "make doc"
     system "make install"
