2cc454dfbd059fa1bdd88d333901a0d0b2551ec0
Carlo Pinciroli <cpinciro@ulb.ac.be>
2016-08-31 16:13:26 -0400

    Formula for new version of ARGoS3 (3.0.0-beta43)

--- argos3.rb
+++ argos3.rb
@@ -4,12 +4,12 @@ class Argos3 < Formula
   VERSION_MAJOR   = "3"
   VERSION_MINOR   = "0"
   VERSION_PATCH   = "0"
-  VERSION_RELEASE = "beta42"
+  VERSION_RELEASE = "beta43"
   VERSION_FULL    = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}-#{VERSION_RELEASE}"
   
   homepage "http://www.argos-sim.info/"
   url "https://github.com/ilpincy/argos3/archive/#{VERSION_FULL}.tar.gz"
-  sha256 "892416623213171d35a4df20b50b2f6a2f08e4b408488a1a0f33924a9bf128a9"
+  sha256 "77bcc93112beb818dee6320574e1a853281466675665345396dd541e82c0f681"
   version VERSION_FULL
   head "https://github.com/ilpincy/argos3.git"
 
