80ab68a49ead866102c046dedc9832d25314b986
Carlo Pinciroli <cpinciro@ulb.ac.be>
2017-02-25 15:59:58 -0500

    Formula for new version of ARGoS3 (3.0.0-beta44)

--- argos3.rb
+++ argos3.rb
@@ -4,16 +4,14 @@ class Argos3 < Formula
   VERSION_MAJOR   = "3"
   VERSION_MINOR   = "0"
   VERSION_PATCH   = "0"
-  VERSION_RELEASE = "beta43"
+  VERSION_RELEASE = "beta44"
   VERSION_FULL    = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}-#{VERSION_RELEASE}"
   
   homepage "http://www.argos-sim.info/"
   url "https://github.com/ilpincy/argos3/archive/#{VERSION_FULL}.tar.gz"
-  sha256 "77bcc93112beb818dee6320574e1a853281466675665345396dd541e82c0f681"
+  sha256 "d80c9bdfb6aa3eaf7a8594eaff311c5f6e0bffe949fb6da6e64b7aad6f10676c"
   version VERSION_FULL
   head "https://github.com/ilpincy/argos3.git"
-
-  option 'without-qt', 'Compile without the Qt-OpenGL visualization'
 
   depends_on "pkg-config" => :build
   depends_on "cmake" => :build
@@ -21,17 +19,16 @@ class Argos3 < Formula
   depends_on "asciidoc" => :build
   depends_on "graphviz" => :build
   depends_on "doxygen" => :build
-  depends_on "gsl"
+  depends_on "qt"
   depends_on "libpng"
   depends_on "freeimage"
   depends_on "lua"
-  depends_on "qt" => :recommended
   depends_on "bash-completion" => :recommended
 
   def install
     mkdir "build_simulator"
     cd "build_simulator"
-    system "cmake", "../src", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DARGOS_BUILD_NATIVE=ON", "-DCPACK_PACKAGE_VERSION_MAJOR=#{VERSION_MAJOR}", "-DCPACK_PACKAGE_VERSION_MINOR=#{VERSION_MINOR}", "-DCPACK_PACKAGE_VERSION_PATCH=#{VERSION_PATCH}", "-DCPACK_PACKAGE_RELEASE=#{VERSION_RELEASE}"
+    system "cmake", "../src", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DARGOS_BUILD_NATIVE=ON", "-DCPACK_PACKAGE_VERSION_MAJOR=#{VERSION_MAJOR}", "-DCPACK_PACKAGE_VERSION_MINOR=#{VERSION_MINOR}", "-DCPACK_PACKAGE_VERSION_PATCH=#{VERSION_PATCH}", "-DCPACK_PACKAGE_RELEASE=#{VERSION_RELEASE}", "-DARGOS_BREW_QT_CELLAR=#{HOMEBREW_PREFIX}/Cellar/qt5"
     system "make"
     system "make doc"
     system "make install"
